90d59a6d915ed24d267df43848fb4bc76b705a3c
SIngleFon <timur5356@mail.ru>
2025-04-23 01:01:28 +0300

    v2.4

--- main.py
+++ main.py
@@ -92,6 +92,6 @@ def submit_survey():
 
 
 if __name__ == '__main__':
-    application.run(debug=True, host='0.0.0.0')
-    # application.run(debug=False)
+    # application.run(debug=True, host='0.0.0.0')
+    application.run(debug=False)
 
